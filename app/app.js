84ed3d22716983284053df87c62c42f22ac770d1
susana <spelaez593@misena.edu.co>
2023-03-10 16:04:37 -0500

    testing

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import message from "./config/message"
-const app = express();
+import environment from "./config/enviroment";
 
-app.listen('3000', ()=>{
-    //console.log("hola mundo");
-    message("Hola mundo", "danger")
-});
+const app = express();
+app.set("PORT",process.env.PORT || 3000);
+
+
+export default app;
